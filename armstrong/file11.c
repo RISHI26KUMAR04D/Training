c9af1138253303d4ac26a0b7083b5f25d8d5b466
rishi <dk.rishikumar474@gmail.com>
2023-03-02 12:42:20 +0530

    new commit

---
+++ armstrong/file11.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+#include"file22.c"
+int armstrong(int n);
+int main()
+{
+	int num,res;
+	printf("Enter the number:");
+	scanf("%d",&num);
+	res=armstrong(num);
+	if(res==num)
+	{
+		printf("It is an ARMSTRONG number.\n");
+	}
+	else
+	{
+		printf("It is not an ARMSTRONG number.\n");
+	}
+	return 0;
+}
---
+++ armstrong/file22.c
@@ -0,0 +1,19 @@
+//#include<math.h>
+int armstrong(int n)
+{
+	int r=0,count=0,sum=0;
+	int n1=n;
+	while(n!=0)
+	{
+		n/=10;
+		count++;		
+	}
+	for(int i=0;n1!=0;i++)
+	{
+		r=n1%10;
+		sum=sum+pow(r,count);
+		n1/=10;
+	}
+	
+	return sum;
+}
